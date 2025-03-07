491d10ce8317fc410d5fe2b46df8f8bb7702e8b4
Saubhagya <saubhagyaranjanbehera64@gmail.com>
2025-03-07 11:00:40 +0530

    Refactor contact details from Java to Kotlin

--- app/src/main/java/com/example/contactsmanager/ContactDetails.kt
+++ app/src/main/java/com/example/contactsmanager/ContactDetails.kt
@@ -1,120 +1,131 @@
-package com.example.contactsmanager;
+package com.example.contactsmanager
 
-import android.content.Intent;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.content.DialogInterface
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
+class ContactDetails : AppCompatActivity() {
+    private var editTextName: EditText? = null
+    private var editTextMobile: EditText? = null
+    private var editTextEmail: EditText? = null
+    private var editTextAddress: EditText? = null
+    private var buttonUpdateContact: Button? = null
+    private var buttonDelete: Button? = null
+    private var buttonCall: Button? = null
+    private var buttonEmail: Button? = null
+    private var buttonMessage: Button? = null
+    private var dbHelper: DatabaseHelper? = null
+    private var contactId = -1
 
-public class activity_contact_details extends AppCompatActivity {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_details)
 
-    private EditText editTextName, editTextMobile, editTextEmail, editTextAddress;
-    private Button buttonUpdateContact, buttonDelete, buttonCall, buttonEmail, buttonMessage;
-    private DatabaseHelper dbHelper;
-    private int contactId = -1;
+        dbHelper = DatabaseHelper(this)
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contact_details);
+        editTextName = findViewById<EditText>(R.id.editText_name)
+        editTextMobile = findViewById<EditText>(R.id.editText_mobile)
+        editTextEmail = findViewById<EditText>(R.id.editText_email)
+        editTextAddress = findViewById<EditText>(R.id.editText_address)
+        buttonUpdateContact = findViewById<Button>(R.id.button_update_contact)
+        buttonDelete = findViewById<Button>(R.id.button_delete)
+        buttonCall = findViewById<Button>(R.id.button_call)
+        buttonEmail = findViewById<Button>(R.id.button_email)
+        buttonMessage = findViewById<Button>(R.id.button_message)
 
-        dbHelper = new DatabaseHelper(this);
-
-        editTextName = findViewById(R.id.editText_name);
-        editTextMobile = findViewById(R.id.editText_mobile);
-        editTextEmail = findViewById(R.id.editText_email);
-        editTextAddress = findViewById(R.id.editText_address);
-        buttonUpdateContact = findViewById(R.id.button_update_contact);
-        buttonDelete = findViewById(R.id.button_delete);
-        buttonCall = findViewById(R.id.button_call);
-        buttonEmail = findViewById(R.id.button_email);
-        buttonMessage = findViewById(R.id.button_message);
-
-        contactId = getIntent().getIntExtra("CONTACT_ID", -1);
-        Log.d("activity_contact_details", "Received CONTACT_ID: " + contactId);
+        contactId = getIntent().getIntExtra("CONTACT_ID", -1)
+        Log.d("activity_contact_details", "Received CONTACT_ID: " + contactId)
 
         if (contactId != -1) {
-            loadContactDetails(contactId);
+            loadContactDetails(contactId)
         }
 
-        buttonUpdateContact.setOnClickListener(v -> {
-            String name = editTextName.getText().toString();
-            String mobile = editTextMobile.getText().toString();
-            String email = editTextEmail.getText().toString();
-            String address = editTextAddress.getText().toString();
+        buttonUpdateContact!!.setOnClickListener(View.OnClickListener { v: View? ->
+            val name = editTextName!!.getText().toString()
+            val mobile = editTextMobile!!.getText().toString()
+            val email = editTextEmail!!.getText().toString()
+            val address = editTextAddress!!.getText().toString()
 
             if (contactId == -1) {
-                dbHelper.addContact(name, mobile, email, address);
+                dbHelper!!.addContact(name, mobile, email, address)
             } else {
-                dbHelper.updateContact(contactId, name, mobile, email, address);
+                dbHelper!!.updateContact(contactId, name, mobile, email, address)
             }
 
-            setResult(RESULT_OK);
-            finish();
-        });
+            setResult(RESULT_OK)
+            finish()
+        })
 
-        buttonDelete.setOnClickListener(v -> {
-            new AlertDialog.Builder(this)
-                    .setTitle("Delete Contact")
-                    .setMessage("Are you sure you want to delete this contact?")
-                    .setPositiveButton(android.R.string.yes, (dialog, which) -> {
-                        dbHelper.deleteContact(contactId);
-                        Toast.makeText(activity_contact_details.this, "Contact deleted", Toast.LENGTH_SHORT).show();
-                        setResult(RESULT_OK);
-                        finish();
+        buttonDelete!!.setOnClickListener(View.OnClickListener { v: View? ->
+            AlertDialog.Builder(this)
+                .setTitle("Delete Contact")
+                .setMessage("Are you sure you want to delete this contact?")
+                .setNegativeButton("Cancel", null)
+                .setPositiveButton(
+                    "Yes",
+                    DialogInterface.OnClickListener { dialog: DialogInterface?, which: Int ->
+                        dbHelper!!.deleteContact(contactId)
+                        Toast.makeText(
+                            this@ContactDetails,
+                            "Contact deleted",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        setResult(RESULT_OK)
+                        finish()
                     })
-                    .setNegativeButton(android.R.string.no, null)
-                    .setIcon(android.R.drawable.ic_dialog_alert)
-                    .show();
-        });
+                .setIcon(R.drawable.delete)
+                .show()
+        })
 
-        buttonCall.setOnClickListener(v -> {
-            String mobile = editTextMobile.getText().toString();
+        buttonCall!!.setOnClickListener(View.OnClickListener { v: View? ->
+            val mobile = editTextMobile!!.getText().toString()
             if (!mobile.isEmpty()) {
-                Intent callIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + mobile));
-                startActivity(callIntent);
+                val callIntent = Intent(Intent.ACTION_DIAL, ("tel:$mobile").toUri())
+                startActivity(callIntent)
             } else {
-                Toast.makeText(this, "Mobile number is empty", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Mobile number is empty", Toast.LENGTH_SHORT).show()
             }
-        });
+        })
 
-        buttonEmail.setOnClickListener(v -> {
-            String email = editTextEmail.getText().toString();
+        buttonEmail!!.setOnClickListener(View.OnClickListener { v: View? ->
+            val email = editTextEmail!!.getText().toString()
             if (!email.isEmpty()) {
-                Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:" + email));
-                startActivity(emailIntent);
+                val emailIntent = Intent(Intent.ACTION_SENDTO, ("mailto:$email").toUri())
+                startActivity(emailIntent)
             } else {
-                Toast.makeText(this, "Email address is empty", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Email address is empty", Toast.LENGTH_SHORT).show()
             }
-        });
+        })
 
-        buttonMessage.setOnClickListener(v -> {
-            String mobile = editTextMobile.getText().toString();
+        buttonMessage!!.setOnClickListener(View.OnClickListener { v: View? ->
+            val mobile = editTextMobile!!.getText().toString()
             if (!mobile.isEmpty()) {
-                Intent smsIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:" + mobile));
-                startActivity(smsIntent);
+                val smsIntent = Intent(Intent.ACTION_SENDTO, ("smsto:$mobile").toUri())
+                startActivity(smsIntent)
             } else {
-                Toast.makeText(this, "Mobile number is empty", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Mobile number is empty", Toast.LENGTH_SHORT).show()
             }
-        });
+        })
     }
 
-    private void loadContactDetails(int id) {
-        Cursor cursor = dbHelper.getContactById(id);
+    private fun loadContactDetails(id: Int) {
+        val cursor = dbHelper!!.getContactById(id)
         if (cursor != null && cursor.moveToFirst()) {
-            editTextName.setText(cursor.getString(cursor.getColumnIndex("name")));
-            editTextMobile.setText(cursor.getString(cursor.getColumnIndex("mobile")));
-            editTextEmail.setText(cursor.getString(cursor.getColumnIndex("email")));
-            editTextAddress.setText(cursor.getString(cursor.getColumnIndex("address")));
-            cursor.close();
+            editTextName!!.setText(cursor.getString(cursor.getColumnIndex("name")))
+            editTextMobile!!.setText(cursor.getString(cursor.getColumnIndex("mobile")))
+            editTextEmail!!.setText(cursor.getString(cursor.getColumnIndex("email")))
+            editTextAddress!!.setText(cursor.getString(cursor.getColumnIndex("address")))
+            cursor.close()
         } else {
-            Log.e("activity_contact_details", "Failed to load contact details for ID: " + id);
+            Log.e("activity_contact_details", "Failed to load contact details for ID: $id")
         }
     }
 }
