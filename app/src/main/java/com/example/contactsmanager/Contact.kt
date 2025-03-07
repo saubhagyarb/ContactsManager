efd58519b495a0c86121766afa05ec691106b0f7
Saubhagya <saubhagyaranjanbehera64@gmail.com>
2025-03-07 11:00:49 +0530

    Refactor contact details from Java to Kotlin

--- app/src/main/java/com/example/contactsmanager/Contact.kt
+++ app/src/main/java/com/example/contactsmanager/Contact.kt
@@ -1,37 +1,9 @@
-package com.example.contactsmanager;
+package com.example.contactsmanager
 
-public class Contact {
-    private int id;
-    private String name;
-    private String mobile;
-    private String email;
-    private String address;
-
-    public Contact(int id, String name, String mobile, String email, String address) {
-        this.id = id;
-        this.name = name;
-        this.mobile = mobile;
-        this.email = email;
-        this.address = address;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getMobile() {
-        return mobile;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-}
+class Contact(
+    @JvmField val id: Int,
+    @JvmField val name: String?,
+    val mobile: String?,
+    val email: String?,
+    val address: String?
+)
